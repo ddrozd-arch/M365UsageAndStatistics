f69b3d17da012894270f39f5405e425bdd6561f0
ddrozd-arch <dawid.drozd@icloud.com>
2026-04-26 19:00:48 +0200

    Create RunResults.cs

---
+++ RunResults.cs
@@ -0,0 +1,7 @@
+public class RunResult
+{
+    public DateTime Date { get; set; }
+    public string Job { get; set; }
+    public string Status { get; set; }
+    public string Details { get; set; }
+}
